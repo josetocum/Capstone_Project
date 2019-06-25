08fc87c255a7764a58da99da64bc8e0bdbf0d6b1
josetocum <51421385+josetocum@users.noreply.github.com>
2019-06-25 11:43:23 -0400

    create k-means

---
+++ k-means.py
@@ -0,0 +1,12 @@
+import random # library for random number generation
+import numpy as np # library for vectorized computation
+import pandas as pd # library to process data as dataframes
+
+import matplotlib.pyplot as plt # plotting library
+# backend for rendering plots within the browser
+%matplotlib inline 
+
+from sklearn.cluster import KMeans 
+from sklearn.datasets.samples_generator import make_blobs
+
+print('Libraries imported.')
